76daf5d091f98391b87b207c23cf2f38c2cffaae
Sara Palmer <sara.palmer@emory.edu>
2016-06-09 15:53:31 -0400

    update

--- make_xml.py
+++ make_xml.py
@@ -32,23 +32,29 @@ with open(infile, 'rU') as csvfile:
 
         # change keys to preferred metadata elements
         metadata_elements = {'code': 'Code', 
-                            'recipient': 'Recipient',
-                            'physical_description': 'PhysDes',
-                            'leaves_sides': 'LeavesSides',
-                            'envelope': 'Envelope',
-                            'postmark': 'Postmark',
-                            'respository': 'Repository',
-                            'place_written': 'PlaceWritten',
-                            'place_sent': 'PlaceSent',
-                            'primary_language': 'PrimaryLang'}
+                            'recipient': 'Recipient'}
+                            # 'physical_description': 'PhysDes',
+                            # 'leaves_sides': 'LeavesSides',
+                            # 'envelope': 'Envelope',
+                            # 'postmark': 'Postmark',
+                            # 'respository': 'Repository',
+                            # 'place_written': 'PlaceWritten',
+                            # 'place_sent': 'PlaceSent',
+                            # 'primary_language': 'PrimaryLang'}
 
         for k in metadata_elements.keys():
             element = ET.SubElement(metadata, k)
             element.text = row[metadata_elements[k]]
 
+        #h/t http://stackoverflow.com/a/9092358
+        declaration = '''<?xml version="1.0" encoding="utf-8"?>
+<?xml-model href="https://raw.githubusercontent.com/emory-libraries-ecds/beckett-schema/master/beckett.rnc"?>
+<?xml-stylesheet type="text/css" href="https://raw.githubusercontent.com/emory-libraries-ecds/beckett-schema/master/beckett.css"?>\n'''
+
         tree = ET.ElementTree(letter)
         out = os.path.join(outdir, os.path.basename(filename + '.xml'))
         with open(out, 'w') as outfile:
-            tree.write(outfile, encoding="utf8", xml_declaration=True)
+            outfile.write(declaration)
+            tree.write(outfile, xml_declaration=False, encoding='utf-8') 
             outfile.close()
 
